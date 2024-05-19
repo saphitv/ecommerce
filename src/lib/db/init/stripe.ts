162a1be29373c60340f44dd7dec0d770837d1c3c
saphitv <simonmaggini@gmail.com>
2024-05-19 12:10:27 +0200

    production mode on (disabled local)

--- src/lib/db/init/stripe.ts
+++ src/lib/db/init/stripe.ts
@@ -8,9 +8,9 @@ const getServerStripe = () => {
 };
 
 const client = createClient({
-    url: 'http://127.0.0.1:8080',
-    //url: process.env.TURSO_CONNECTION_URL!,
-    //authToken: process.env.TURSO_AUTH_TOKEN!,
+    //url: 'http://127.0.0.1:8080',
+    url: process.env.TURSO_CONNECTION_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN!,
 })
 
 const db = drizzle(client, {schema: schema})
